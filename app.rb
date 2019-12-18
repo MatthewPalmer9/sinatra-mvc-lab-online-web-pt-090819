7c4aa8191d351ec2104b85cabf0eff280dd6cc6c
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-12-18 04:25:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
-    @piglatinizer = PigLatinizer.new.toPigLatin(params[:user_phrase].to_s)
-    erb :piglatinize 
+    @piglatinizer = PigLatinizer.new.piglatinize(params[:user_phrase].to_s)
+    erb :piglatinize
   end
 end
