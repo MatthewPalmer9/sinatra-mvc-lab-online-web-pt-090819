8059a2e3f270a352dafad5b11351c1d8240ab106
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-12-18 04:15:36 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,7 +1,7 @@
 class PigLatinizer
 
   def piglatinize(word)
-    if !self.vowel?(word)
+    if self.vowel?(word)
       word.concat('way')
     else
       word.concat(word.slice!(/^[^aeiouAEIOU]*/i || "")) + 'ay'
