d0b01854d96be6fae1d7eddf7876f5cee7869a38
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-12-18 03:54:01 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -10,7 +10,7 @@ class PigLatinizer
         letters << letters.shift
       end
       letters << "ay"
-    elsif letters.size == 1
+    elsif letters.first.scan(/[aeiou]/)
       letters[0] << "way"
     end
 
