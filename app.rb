0f3ae1793005b83cc78d14fa73a2df1c434c2bb5
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-12-18 03:25:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/' do
+    @piglatinizer = PigLatinizer.new.toPigLatin(params[:user_input].to_s)
+    erb :result
+  end
 end
